54dccb745004b2b3f5577a8dd4b83518e48ea313
gmnayem631 <gmnayem631@gmail.com>
2026-02-13 09:26:15 +0600

    added the header of let's learn vocabulary section

---
+++ script/index.js
@@ -0,0 +1,23 @@
+const loadLessons = () => {
+  fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then((response) => response.json())
+    .then((json) => {
+      displayLessons(json.data);
+    });
+};
+
+const displayLessons = (lessons) => {
+  const levelContainer = document.getElementById("level-container");
+  levelContainer.innerHTML = "";
+
+  lessons.forEach((lesson) => {
+    const btnDiv = document.createElement("div");
+    btnDiv.innerHTML = `
+    <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}</button>
+    `;
+
+    levelContainer.appendChild(btnDiv);
+  });
+};
+
+loadLessons();
